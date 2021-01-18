51a793fa0eb13bd1f2ef000ce82fe0c17c3bfadb
Johannes Ott <info@johannes-ott.net>
2021-01-18 20:19:19 +0100

    Color replacement and png convert
    Signed-off-by: Johannes Ott <info@johannes-ott.net>

---
+++ openhab_creator/output/color.py
@@ -0,0 +1,16 @@
+from enum import Enum
+
+
+class Color(Enum):
+    GREEN = "#00c83c"
+    DARKGREEN = "#00b22a"
+    RED = "#f00014"
+    DARKRED = "#d6001a"
+    ORANGE = "#ff9900"
+    YELLOW = "#ffff00"
+    BLUE = "#0066ff"
+    LIGHTGREY = "#f0f0f0"
+    DARKGREY = "#999999"
+    GREY = "#cecece"
+    VIOLETT = "#8942f4"
+    BLACK = "#000000"
--- openhab_creator/output/iconscreator.py
+++ openhab_creator/output/iconscreator.py
@@ -1,6 +1,15 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+
 import os
 import shutil
 from pathlib import Path
+from cairosvg import svg2png
+
+if TYPE_CHECKING:
+    from posix import DirEntry
+
+from openhab_creator.output.color import Color
 
 
 class IconsCreator(object):
@@ -13,19 +22,20 @@ class IconsCreator(object):
         pwd = Path(__file__).resolve().parent.parent
         src_dir = f'{pwd}/content/icons'
 
-        for dir in os.scandir(src_dir):
-            icon_basename = dir.name
-            for file in os.scandir(dir.path):
-                if 'default.svg' == file.name:
-                    icon_name = f'{icon_basename}.svg'
-                elif file.name.endswith('.svg'):
-                    icon_name = f'{icon_basename}-{file.name}'
+        for category in os.scandir(src_dir):
+            icon_basename = category.name
+            for srcfile in os.scandir(category.path):
+                if 'default.svg' == srcfile.name:
+                    icon_name = f'{icon_basename}'
+                elif srcfile.name.endswith('.svg'):
+                    icon_name = f'{icon_basename}-{srcfile.name}'[0:-4]
                 else:
                     continue
 
-                print(f'Creating {icon_name}')
-                destination = os.path.join(self._outputdir, icon_name)
-                shutil.copy(file.path, destination)
+                print(f'Create icon: {icon_name}')
+                content = self.__generate_output_icons(srcfile)
+                self.__write_svg(icon_name, content)
+                self.__convert_write_png(icon_name, content)
 
     def __make_clean_destination_directory(self):
         print('Clean icons directory')
@@ -34,3 +44,21 @@ class IconsCreator(object):
                 os.remove(file.path)
         else:
             os.makedirs(self._outputdir)
+
+    def __generate_output_icons(self, srcfile: DirEntry) -> str:
+        with open(srcfile, 'r') as f:
+            content = f.read()
+            for color in Color:
+                content = content.replace(f'__{color.name}__', color.value)
+
+        return content
+
+    def __write_svg(self, icon_basename: str, content: str) -> None:
+        destination = os.path.join(self._outputdir, f'{icon_basename}.svg')
+        with open(destination, 'w') as f:
+            f.write(content)
+
+    def __convert_write_png(self, icon_basename, content: str) -> None:
+        destination = os.path.join(self._outputdir, f'{icon_basename}.png')
+        svg2png(bytestring=content, write_to=destination,
+                output_height=32, output_width=32)
