cdb2075ddcfca0fb3aeb5aaf389157a904e4c285
Johannes Ott <info@johannes-ott.net>
2021-06-01 22:54:06 +0200

    Added weatherstation temperature and rain gauge
    Signed-off-by: Johannes Ott <info@johannes-ott.net>

--- openhab_creator/output/items/creators/weatherstationitemscreator.py
+++ openhab_creator/output/items/creators/weatherstationitemscreator.py
@@ -6,7 +6,7 @@ from openhab_creator import _
 from openhab_creator.models.common import MapTransformation
 from openhab_creator.models.configuration.equipment.types.weatherstation import \
     WeatherStationType
-from openhab_creator.models.items import (Group, Number, PointType,
+from openhab_creator.models.items import (Group, GroupType, Number, PointType,
                                           ProfileType, String)
 from openhab_creator.output.items import ItemsCreatorPipeline
 from openhab_creator.output.items.baseitemscreator import BaseItemsCreator
@@ -19,6 +19,10 @@ if TYPE_CHECKING:
 
 @ItemsCreatorPipeline(7)
 class WeatherStationItemsCreator(BaseItemsCreator):
+    def __init__(self, outputdir: str):
+        super().__init__(outputdir)
+        self.groups = {}
+
     def build(self, configuration: Configuration) -> None:
         String('weatherstation')\
             .label(_('Weather station'))\
@@ -46,12 +50,22 @@ class WeatherStationItemsCreator(BaseItemsCreator):
 
         for weathertype in WeatherStationType:
             if station.points.has(weathertype.point):
-                Number(f'{weathertype}{station.item_ids.sensor}')\
+                if weathertype not in self.groups:
+                    Group(f'{weathertype}WeatherStation')\
+                        .typed(GroupType.NUMBER_AVG)\
+                        .label(weathertype.labels.page)\
+                        .format(weathertype.labels.format_str)\
+                        .icon(f'{weathertype}')\
+                        .append_to(self)
+
+                    self.groups[weathertype] = True
+
+                Number(f'{weathertype}{station.identifier}')\
                     .typed(weathertype.typed.number)\
                     .label(weathertype.labels.item)\
                     .format(weathertype.labels.format_str)\
                     .icon(f'{weathertype}')\
-                    .groups(station.item_ids.merged_sensor)\
+                    .groups(station.item_ids.merged_sensor, f'{weathertype}WeatherStation')\
                     .semantic(PointType.MEASUREMENT, weathertype.typed.property)\
                     .channel(station.points.channel(weathertype.point))\
                     .sensor(weathertype.point, station.influxdb_tags)\
--- openhab_creator/output/sitemap/creators/weatherstationsitemapcreator.py
+++ openhab_creator/output/sitemap/creators/weatherstationsitemapcreator.py
@@ -1,7 +1,8 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Callable, List
 
+from openhab_creator import _
 from openhab_creator.models.sitemap import Page, Text, Sitemap
 from openhab_creator.output.sitemap import SitemapCreatorPipeline
 from openhab_creator.output.sitemap.basesitemapcreator import \
@@ -9,6 +10,7 @@ from openhab_creator.output.sitemap.basesitemapcreator import \
 
 if TYPE_CHECKING:
     from openhab_creator.models.configuration import Configuration
+    from openhab_creator.models.configuration.equipment import Equipment
     from openhab_creator.models.grafana import Dashboard
 
 
@@ -25,6 +27,56 @@ class WeatherStationSitemapCreator(BaseSitemapCreator):
         for condition in configuration.equipment.equipment('condition_id'):
             Text(condition.item_ids.condition)\
                 .append_to(weatherstation)
+
+        self._build_temperatures(weatherstation, configuration)
+        self._build_rain_gauge(weatherstation, configuration)
+
+    def _build_temperatures(self, weatherstation_page: Page, configuration: Configuration) -> None:
+        temperatures = self.filter_stations(lambda x: x.points.has_temperature,
+                                            configuration)
+
+        if len(temperatures) > 0:
+            page = Page('temperatureOutdoor')\
+                .label(_('Temperatures'))\
+                .append_to(weatherstation_page)
+
+            locations = []
+
+            for temperature in temperatures:
+                locations.append(temperature.location.toplevel)
+                Text(temperature.item_ids.temperature)\
+                    .append_to(page)
+
+                if temperature.points.has_humidex:
+                    Text(temperature.item_ids.humidex)\
+                        .append_to(page)
+
+            self._add_grafana(configuration.dashboard, page,
+                              list(dict.fromkeys(locations)),
+                              _('Temperatures') + ' ')
+
+    def _build_rain_gauge(self, weatherstation_page: Page, configuration: Configuration) -> None:
+        rain_gauges = self.filter_stations(
+            lambda x: x.points.has_rain_gauge, configuration)
+
+        if len(rain_gauges) > 0:
+            page = Page('rain_gaugeWeatherStation')\
+                .append_to(weatherstation_page)
+
+            locations = []
+
+            for rain_gauge in rain_gauges:
+                locations.append(rain_gauge.location.toplevel)
+                Text(rain_gauge.item_ids.rain_gauge)\
+                    .append_to(page)
+
+            self._add_grafana(configuration.dashboard, page,
+                              list(dict.fromkeys(locations)),
+                              _('Rain gauge') + ' ')
+
+    @staticmethod
+    def filter_stations(filter_func: Callable, configuration: Configuration) -> List[Equipment]:
+        return list(filter(filter_func, configuration.equipment.equipment('weatherstation')))
 
     def build_statuspage(self, statuspage: Page, configuration: Configuration) -> None:
         """No status page for weatherstation"""
