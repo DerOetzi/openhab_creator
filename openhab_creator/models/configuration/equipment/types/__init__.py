2bf39497bff9ce9e5b2690f393cecefd3afedf96
Johannes Ott <info@johannes-ott.net>
2021-07-31 22:24:40 +0200

    Bugfix module import in 3.8.10
    Signed-off-by: Johannes Ott <info@johannes-ott.net>

--- openhab_creator/models/configuration/equipment/types/__init__.py
+++ openhab_creator/models/configuration/equipment/types/__init__.py
@@ -1,11 +1,11 @@
-from inspect import isclass
-from pkgutil import iter_modules
-from pathlib import Path
+import os
 from importlib import import_module
+from inspect import isclass
+from pathlib import Path
+from pkgutil import iter_modules
 
 package_dir = Path(__file__).resolve().parent
-for (_, module_name, _) in iter_modules([package_dir]):
-
+for (_, module_name, _) in iter_modules([os.fsdecode(package_dir)]):
     module = import_module(f"{__name__}.{module_name}")
     for attribute_name in dir(module):
         attribute = getattr(module, attribute_name)
--- openhab_creator/output/sitemap/creators/__init__.py
+++ openhab_creator/output/sitemap/creators/__init__.py
@@ -1,11 +1,12 @@
-from inspect import isclass
-from pkgutil import iter_modules
-from pathlib import Path
+import os
 from importlib import import_module
+from inspect import isclass
+from pathlib import Path
+from pkgutil import iter_modules
 
 # iterate through the modules in the current package
 package_dir = Path(__file__).resolve().parent
-for (_, module_name, _) in iter_modules([package_dir]):
+for (_, module_name, _) in iter_modules([os.fsdecode(package_dir)]):
 
     # import the module and iterate through its attributes
     module = import_module(f"{__name__}.{module_name}")
