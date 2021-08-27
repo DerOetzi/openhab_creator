e92dcd5ebe26f56592edd27279573d373d09f278
Johannes Ott <mail@johannes-ott.net>
2021-08-27 21:40:53 +0200

    Change or reactivates scenes when events are triggered

--- openhab_creator/content/automation/libraries/scenemanager.py
+++ openhab_creator/content/automation/libraries/scenemanager.py
@@ -181,7 +181,7 @@ class SceneManager(object):
             else:
                 actual_scene = SpecialScene.ABSENCE
         else:
-            actual_scene_string = SceneItem.auto_scene.get_string()
+            actual_scene_string = SceneItem.auto_scene_active.get_string()
             actual_scene = TimeScene.from_string(actual_scene_string)
             if actual_scene is None:
                 actual_scene = SpecialScene.from_string(actual_scene_string)
@@ -222,15 +222,22 @@ class SceneManager(object):
     def activate_scene(self, event=None):
         SceneItem.update_event(event)
         guest_stayed = SceneItem.guest_stayed.get_onoff(True)
+        is_weekend = EphemerisUtils.is_weekend() or EphemerisUtils.is_holiday()
 
         for assigned_item in self.scene_members:
             active_item = assigned_item.from_scripting('active_item', event)
+
             guest_item = assigned_item.from_scripting('guest_item', event)
             guest_only = guest_item.get_onoff(True)
 
+            weekend_item = assigned_item.from_scripting('weekend_item', event)
+            weekend_active = weekend_item.get_onoff(True, ON, event)
+
             if (assigned_item.get_onoff(True, event=event)
                     and (guest_stayed
-                         or not (guest_stayed or guest_only))):
+                         or not (guest_stayed or guest_only))
+                    and (weekend_active
+                         or not (weekend_active or is_weekend))):
                 active_item.post_update(ON)
             else:
                 active_item.post_update(OFF)
--- openhab_creator/content/automation/rules/scene.py
+++ openhab_creator/content/automation/rules/scene.py
@@ -15,6 +15,25 @@ def scene_startup(event):
     manager.change_scene()
 
 
+@rule('Change scene')
+@when('Item autoScene received command ON')
+@when('Item autoSceneActive received command')
+@when('Item autoGuestStayed received command')
+@when('Descendent of Presences changed')
+@when('Item wayhome received command')
+def change_scene(event):
+    manager = SceneManager.instance()
+    manager.change_scene(event)
+
+
+@rule('Activate scene')
+@when('Descendent of Auto received command')
+@when('Item darkness changed')
+def activate_scene(event):
+    manager = SceneManager.instance()
+    manager.activate_scene(event)
+
+
 def scriptUnloaded():  # NOSONAR
     manager = SceneManager.instance()
     manager.clear_timer()
